7353b2fa6b0f40d852ab1a6c0e74536f33dc7165
liyy0 <yuyanli@bu.edu>
2024-02-15 00:37:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yuyanli_assignment3q2/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.yuyanli_assignment3q2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "ActivityLifecycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+        var bnt = findViewById<Button>(R.id.button)
+        bnt.setOnClickListener {
+            // “Hello” should appear in both EditText and the TextView
+            var editText = findViewById<EditText>(R.id.editTextText)
+            var textView = findViewById<TextView>(R.id.textView)
+            textView.text = "Hello"
+            editText.setText("Hello")
+
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+}
